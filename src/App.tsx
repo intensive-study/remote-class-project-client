ce98412f1088313f617ea33123af37f5bc964571
Jong Woo Park <jwmtp2@gmail.com>
2022-02-10 23:56:09 +0900

    소켓 테스트 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Main from './components/pages/Main';
 import Join from './components/pages/Join';
 import Login from './components/pages/Login';
 import Lecture from './components/pages/Lecture';
+import Test from './components/pages/Test';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path='/join' element={<Join />} />
       <Route path='/login' element={<Login />} />
       <Route path='/lecture' element={<Lecture />} />
+      <Route path='/test' element={<Test />} />
     </Routes>
   );
 }
--- src/components/pages/Lecture/index.tsx
+++ src/components/pages/Lecture/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import '../../../libs/lecture';
-
 export default function Lecture() {
   return <></>;
 }
